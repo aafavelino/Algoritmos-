25e73756d714e6e75c3ade2845cac68d6d866005
Adelino Afonso <aafavelino@ufrn.edu.br>
2016-09-07 12:01:47 -0300

    Update K_esimo_menor.cpp

--- K_esimo_menor.cpp
+++ K_esimo_menor.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 	using namespace std;
 
-		int preenche(int *v, int n);
-		int ordena(int *vetor, int tamanho);
+
+		int preenche(int *v, int n){
+			for (int i = 0; i < n; ++i)
+				std::cin >> v[i];
+			return *v;
+		}
+
+		int ordena(int *vetor, int tamanho){
+		
+			for (int i = tamanho - 1; i > 0 ; --i)
+				for (int j = 0; j < tamanho; ++j)
+					if (vetor[j] > vetor[j+1])
+						 swap(vetor[j],vetor[j+1]);
+		}
 
 		int main(int argc, char const *argv[])
 		{
@@ -31,16 +43,3 @@
 			return EXIT_SUCCESS;
 		}
 
-		int preenche(int *v, int n){
-			for (int i = 0; i < n; ++i)
-				std::cin >> v[i];
-			return *v;
-		}
-
-		int ordena(int *vetor, int tamanho){
-		
-			for (int i = tamanho - 1; i > 0 ; --i)
-				for (int j = 0; j < tamanho; ++j)
-					if (vetor[j] > vetor[j+1])
-						 swap(vetor[j],vetor[j+1]);
-		}
